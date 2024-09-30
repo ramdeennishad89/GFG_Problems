b347dd5daf2d5734f2b94d47305350a63aa5b063
Ramdeen Nishad <88136985+ramdeennishad89@users.noreply.github.com>
2024-09-30 09:23:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two BST 's/merge-two-bst-s.java
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.java
@@ -111,16 +111,52 @@ class Solution {
     // Function to return a list of integers denoting the node
     // values of both the BST in a sorted order.
     public List<Integer> merge(Node root1, Node root2) {
-    List<Integer> list = new ArrayList<>();
-        helper(root1, list);
-        helper(root2, list);
-        Collections.sort(list);
-        return list;
+        // Write your code here
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        
+        inOrderTraversal(root1, list1);
+        inOrderTraversal(root2, list2);
+        
+        return mergedSortedList(list1, list2);
     }
-    void helper(Node root, List<Integer> list){
-        if (root == null) return;
+    
+    private void inOrderTraversal(Node root, List<Integer> list){
+        if(root==null){
+            return;
+        }
+        
+        inOrderTraversal(root.left, list);
         list.add(root.data);
-        helper(root.right, list);
-        helper(root.left, list);
+        inOrderTraversal(root.right, list);
+    }
+    
+    private List<Integer> mergedSortedList(List<Integer> list1, List<Integer> list2) {
+        List<Integer> mergedList = new ArrayList<>();
+        int i = 0, j = 0;
+
+        while (i < list1.size() && j < list2.size()) {
+            if (list1.get(i) <= list2.get(j)) {
+                mergedList.add(list1.get(i));
+                i++;
+            } else {
+                mergedList.add(list2.get(j));
+                j++;
+            }
+        }
+
+        // Add remaining elements from list1
+        while (i < list1.size()) {
+            mergedList.add(list1.get(i));
+            i++;
+        }
+
+        // Add remaining elements from list2
+        while (j < list2.size()) {
+            mergedList.add(list2.get(j));
+            j++;
+        }
+
+        return mergedList;
     }
 }
